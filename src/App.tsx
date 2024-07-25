078c2973c991d57aefa553ef1d1b3797a5f29cc7
eg-bond <eg-bond@yandex.ru>
2024-07-25 15:46:21 +0300

    dock: initial project setup

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { MantineProvider } from '@mantine/core';
+import '@mantine/core/styles.css';
+
+function App() {
+  return (
+    <MantineProvider>
+      <div>Hello</div>
+    </MantineProvider>
+  );
+}
+
+export default App;
